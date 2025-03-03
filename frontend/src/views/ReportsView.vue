1997a81a7734c9f646c8dcf5ed2af39e6a2c8149
keannugran19 <main.grankeannu@gmail.com>
2025-03-03 09:45:16 +0800

    UPDATE reports page design

--- frontend/src/views/ReportsView.vue
+++ frontend/src/views/ReportsView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import ReportsView from '@/components/reports/ReportsTable.vue'
+import ReportsTable from '@/components/reports/ReportsTable.vue'
 </script>
 
 <template>
-  <main class="flex-1 p-4">
-    <ReportsView />
+  <main class="flex-1 p-3">
+    <div class="p-5 max-w-7xl mx-auto bg-white rounded-lg shadow-sm">
+      <h2 class="text-2xl font-semibold text-gray-800 mb-3">Crab Classification Records</h2>
+      <ReportsTable />
+    </div>
   </main>
 </template>
